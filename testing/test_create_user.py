8e4835af36ddf8dcf3478ee0b90a7d3e43d5be4c
andresDoctors <andresdaviddoctors@gmail.com>
2023-11-18 18:39:10 -0300

    Crear Usuario

--- testing/test_create_user.py
+++ testing/test_create_user.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi import HTTPException
 from fastapi.testclient import TestClient
 
+from src import app
 from testing import get_test_db
 from src.database import UsersTable, GamesTable, CardsTable, ShowedCardsTable, __SessionLocal__
 from src.endpoints.create_game import create_game, CreateGameModel
@@ -18,4 +19,15 @@ from src.endpoints.start_game import start_game
 
 def test_create_user(get_test_db):
     db = get_test_db
-    pass
+    client = TestClient(app)
+
+    nickname = "andy"
+    response = client.post(url=f"/users?nickname={nickname}")
+    assert response.status_code == 200
+    body = response.json()
+    assert body["nickname"] == "andy"
+    assert all((key == "nickname" or body[key] is None) for key in body)
+
+    response = client.post(url=f"/users?nickname={nickname}")
+    assert response.status_code == 409
+    assert response.json()["detail"] == "Ya existe un Usuario con ese nickname"
