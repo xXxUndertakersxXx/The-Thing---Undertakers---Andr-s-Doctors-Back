4d46f3df624bf0cdd4bca8850342eb4aae181208
andresDoctors <andresdaviddoctors@gmail.com>
2023-11-18 20:53:26 -0300

    Listar Partidas

--- src/endpoints/list_games.py
+++ src/endpoints/list_games.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException, Depends, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
+from typing import List
 
 from src import app
 from src.database import UsersTable, GamesTable, CardsTable, ShowedCardsTable, get_db
@@ -9,4 +10,24 @@ from pydantic import BaseModel
 
 @app.get("/games")
 def list_games(db: Session = Depends(get_db)):
-    pass
+    games: List[GamesTable] = (
+        db
+        .query(GamesTable)
+        .filter(GamesTable.phase == 'Waiting')
+        .filter(GamesTable.player_count < GamesTable.max_players)
+        .all()
+    )
+
+    return JSONResponse(content={
+        'games': [
+            {
+                "name": game.name,
+                "creator": game.creator,
+                "users_count": len(db.query(UsersTable).filter(UsersTable.game_name == game.name).all()),
+                "users_in": [user.game_name for user in db.query(UsersTable).filter(UsersTable.game_name == game.name).all()],
+                "min_players": game.min_players,
+                "max_players": game.max_players
+            }
+            for game in games
+        ]
+    })
--- testing/test_list_games.py
+++ testing/test_list_games.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi import HTTPException
 from fastapi.testclient import TestClient
 
+from src import app
 from testing import get_test_db
 from src.database import UsersTable, GamesTable, CardsTable, ShowedCardsTable, __SessionLocal__
 from src.endpoints.create_game import create_game, CreateGameModel
@@ -18,4 +19,50 @@ from src.endpoints.start_game import start_game
 
 def test_list_games(get_test_db):
     db = get_test_db
-    pass
+    client = TestClient(app)
+
+    response = client.get('/games')
+    assert response.status_code == 200
+    assert response.json()['games'] == []
+
+    creator1 = 'andy'
+    response = client.post(f'/users?nickname={creator1}')
+
+    game_name1 = 'Partida de Andy'
+    body = {"game_name": game_name1, "min_players": 4, "max_players": 12, "creator": creator1}
+    response = client.post(url="/games", json=body)
+    game1 = response.json()
+
+    response = client.get('/games')
+    assert response.status_code == 200
+    assert len(response.json()['games']) == 1
+
+    creator2 = 'pepe'
+    response = client.post(f'/users?nickname={creator2}')
+
+    game_name2 = 'Partida de Pepe'
+    body = {"game_name": game_name2, "min_players": 4, "max_players": 12, "creator": creator2}
+    response = client.post(url="/games", json=body)
+    game2 = response.json()
+
+    response = client.get('/games')
+    assert response.status_code == 200
+    assert len(response.json()['games']) == 2
+
+    assert response.json()['games'][0] == {
+        'creator': 'andy',
+        'max_players': 12,
+        'min_players': 4,
+        'name': 'Partida de Andy',
+        'users_count': 0,
+        'users_in': []
+    }
+
+    assert response.json()['games'][1] == {
+        'creator': 'pepe',
+        'max_players': 12,
+        'min_players': 4,
+        'name': 'Partida de Pepe',
+        'users_count': 0,
+        'users_in': []
+    }
