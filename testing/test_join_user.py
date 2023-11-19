26fde4b7a08b5d65b8673f315652b7e3721e0d10
andresDoctors <andresdaviddoctors@gmail.com>
2023-11-18 21:23:09 -0300

    Unir Usuario

--- testing/test_join_user.py
+++ testing/test_join_user.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi import HTTPException
 from fastapi.testclient import TestClient
 
+from src import app
 from testing import get_test_db
 from src.database import UsersTable, GamesTable, CardsTable, ShowedCardsTable, __SessionLocal__
 from src.endpoints.create_game import create_game, CreateGameModel
@@ -18,4 +19,29 @@ from src.endpoints.start_game import start_game
 
 def test_join_user(get_test_db):
     db = get_test_db
-    pass
+    client = TestClient(app)
+
+    nickname = 'andy'
+    game_name = 'LaPartidaDeAndy'
+
+    response = client.patch(f'/users/{nickname}?game_name={game_name}')
+    assert response.status_code == 404
+    assert response.json()['detail'] == f"No existe un Usuario con nickname {nickname}"
+
+    response = client.post(f'/users?nickname={nickname}')
+    response = client.patch(f'/users/{nickname}?game_name={game_name}')
+    assert response.status_code == 404
+    assert response.json()['detail'] == f"No existe una Partida con nombre {game_name}"
+
+    body = {"game_name": game_name, "min_players": 4, "max_players": 12, "creator": nickname}
+    response = client.post(f'/games', json=body)
+    response = client.patch(f'/users/{nickname}?game_name={game_name}')
+    assert response.status_code == 200
+    assert response.json()['user']['nickname'] == nickname
+    assert response.json()['user']['game_name'] == game_name
+    assert response.json()['game']['name'] == game_name
+    assert response.json()['game']['player_count'] == 1
+
+    response = client.patch(f'/users/{nickname}?game_name={game_name}')
+    assert response.status_code == 400
+    assert response.json()['detail'] == f"Usuario {nickname} ya se encuentra en una Partida actualmente"
