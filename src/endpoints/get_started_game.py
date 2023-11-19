e5304e804d25af84910017b6855baadd47525d6f
andresDoctors <andresdaviddoctors@gmail.com>
2023-11-19 13:19:29 -0300

    Obtener Partida Empezada

--- src/endpoints/get_started_game.py
+++ src/endpoints/get_started_game.py
@@ -1,12 +1,57 @@
 from fastapi import HTTPException, Depends, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
+from typing import List
 
 from src import app
 from src.database import UsersTable, GamesTable, CardsTable, ShowedCardsTable, get_db
-from pydantic import BaseModel
-
+from src.CardsMetadata import CARDS_METADATA
 
 @app.get("/games/playing/{nickname}")
 def get_started_game(nickname: str, db: Session = Depends(get_db)):
-    pass
+    user: UsersTable | None = db.get(UsersTable, nickname)
+    if user is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, f"No existe un Usuario con nickname {nickname}")
+    if user.game_name is None:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, f"Usuario {nickname} no se encuentra en una Partida actualmente")
+
+    game: GamesTable = db.get(GamesTable, user.game_name)
+    if game.phase != 'Playing' and game.phase != 'Finished':
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, f"La Partida en la que se encuentre el Usuario {nickname} aún no ha empezado")
+
+    alive_users: List[UsersTable] = (
+        db
+        .query(UsersTable)
+        .filter(UsersTable.game_name == user.game_name)
+        .filter(UsersTable.alive == True)
+        .all()
+    )
+
+    active_user: UsersTable
+    any((active_user := user).active for user in alive_users)
+
+    return JSONResponse(content={
+        'game_name': game.name,
+        'phase': game.phase,
+        'creator': game.creator,
+        'active_player': active_user.nickname,
+        'alive_players': [user.nickname for user in alive_users],
+        'locked_doors': [user.stuck_door for user in alive_users],
+        'quarantines': [user.quarantine for user in alive_users],
+        'player_hand': [
+            {
+                'name': card.name,
+                'description': CARDS_METADATA[card.name]['description']
+            }
+            for card in db.query(CardsTable).filter(CardsTable.owner == nickname).all()
+        ],
+        'visible_cards': [
+            {
+                'name': card.name,
+                'description': CARDS_METADATA[card.name]['description'],
+                'owner': showed_card.owner
+            }
+            for showed_card in db.query(ShowedCardsTable).filter(ShowedCardsTable.viewer == nickname).all()
+            if (card := db.get(CardsTable, showed_card.card_id))
+        ]
+    })
--- testing/test_get_started_game.py
+++ testing/test_get_started_game.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi import HTTPException
 from fastapi.testclient import TestClient
 
+from src import app
 from testing import get_test_db
 from src.database import UsersTable, GamesTable, CardsTable, ShowedCardsTable, __SessionLocal__
 from src.endpoints.create_game import create_game, CreateGameModel
@@ -18,4 +19,45 @@ from src.endpoints.start_game import start_game
 
 def test_get_started_game(get_test_db):
     db = get_test_db
-    pass
+    client = TestClient(app)
+
+    nickname1 = 'andy'
+    response = client.get(f'/games/playing/{nickname1}')
+    assert response.status_code == 404
+    assert response.json()['detail'] == f"No existe un Usuario con nickname {nickname1}"
+
+    response = client.post(f'/users?nickname={nickname1}')
+    response = client.get(f'/games/playing/{nickname1}')
+    assert response.status_code == 400
+    assert response.json()['detail'] == f"Usuario {nickname1} no se encuentra en una Partida actualmente"
+
+    game_name = 'Partida de Andy'
+    body = {"game_name": game_name, "min_players": 4, "max_players": 12, "creator": nickname1}
+    response = client.post(url="/games", json=body)
+    response = client.patch(f'/users/{nickname1}?game_name={game_name}')
+    response = client.get(f'/games/playing/{nickname1}')
+    assert response.status_code == 400
+    assert response.json()['detail'] == f"La Partida en la que se encuentre el Usuario {nickname1} aún no ha empezado"
+
+    nickname2 = 'pepe'
+    nickname3 = 'carla'
+    nickname4 = 'juan'
+    response = client.post(f'/users?nickname={nickname2}')
+    response = client.post(f'/users?nickname={nickname3}')
+    response = client.post(f'/users?nickname={nickname4}')
+    response = client.patch(f'/users/{nickname2}?game_name={game_name}')
+    response = client.patch(f'/users/{nickname3}?game_name={game_name}')
+    response = client.patch(f'/users/{nickname4}?game_name={game_name}')
+    response = client.post(f'/games/{nickname1}')
+    response = client.get(f'/games/playing/{nickname1}')
+    assert response.status_code == 200
+    assert response.json()['game_name'] == game_name
+    assert response.json()['phase'] == 'Playing'
+    assert response.json()['creator'] == nickname1
+    assert response.json()['active_player'] in [nickname1, nickname2, nickname3, nickname4]
+    assert sorted(response.json()['alive_players']) == sorted([nickname1, nickname2, nickname3, nickname4])
+    assert response.json()['locked_doors'] == [False, False, False, False]
+    assert response.json()['quarantines'] == [False, False, False, False]
+    assert len(response.json()['player_hand']) == 4 or response.json()['active_player'] == nickname1
+    assert len(response.json()['player_hand']) == 5 or response.json()['active_player'] != nickname1
+    assert response.json()['visible_cards'] == []
