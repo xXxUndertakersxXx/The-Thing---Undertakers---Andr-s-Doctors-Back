a2ea2a01b9bbf2ac316a7890dcade7234f447988
andresDoctors <andresdaviddoctors@gmail.com>
2023-11-19 19:26:05 -0300

    Jugar Carta

--- src/endpoints/play_card.py
+++ src/endpoints/play_card.py
@@ -3,10 +3,63 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from src import app
+from src.draw_card import draw_card
+from src.CardsMetadata import CARDS_METADATA
 from src.database import UsersTable, GamesTable, CardsTable, ShowedCardsTable, get_db
 from pydantic import BaseModel
 
 
 @app.post("/games/playing/{nickname}")
 def play_card(nickname: str, discard: bool, card_name: str, target: str | None = None, db: Session = Depends(get_db)):
-    pass
+    user: UsersTable | None = db.get(UsersTable, nickname)
+    if user is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, f"No existe un Usuario con nickname {nickname}")
+    if user.game_name is None:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, f"Usuario {nickname} no se encuentra en una Partida actualmente")
+
+    game = db.get(GamesTable, user.game_name)
+    if game.phase == 'Waiting':
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, f"La Partida en la que se encuentre el Usuario {nickname} aún no ha empezado")
+    if game.phase == 'Finished':
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, f"La Partida en la que se encuentre el Usuario {nickname} ya terminó")
+
+    if not user.active:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, f"Usuario {nickname} no es el jugador activo")
+
+    play_response = {}
+    if not discard:
+        target = target if (target is not None) else ""
+        play_response = CARDS_METADATA[card_name].play(nickname, target, game.name, db)
+
+    card: CardsTable = (
+        db
+        .query(CardsTable)
+        .filter(CardsTable.owner == nickname)
+        .filter(CardsTable.name == card_name)
+        .first()
+    )
+    card.owner = 'Graveyard'
+    db.query(ShowedCardsTable).filter(ShowedCardsTable.card_id == card.id).delete()
+
+    next_user = db.get(UsersTable, user.next_user)
+    draw_card(game.name, next_user.nickname, db)
+    user.active = False
+    next_user.active = True
+    db.commit()
+
+    return JSONResponse(content={
+        'play_response': play_response,
+        'game': {c.name: getattr(game, c.name) for c in game.__table__.columns},
+        'users': [
+            {c.name: getattr(user, c.name) for c in user.__table__.columns}
+            for user in db.query(UsersTable).all()
+        ],
+        'cards': [
+            {c.name: getattr(card, c.name) for c in card.__table__.columns}
+            for card in db.query(CardsTable).all()
+        ],
+        'showed_cards': [
+            {c.name: getattr(card, c.name) for c in card.__table__.columns}
+            for card in db.query(CardsTable).all()
+        ],
+    })
--- testing/test_play_card.py
+++ testing/test_play_card.py
@@ -3,6 +3,7 @@ from fastapi import HTTPException
 from fastapi.testclient import TestClient
 
 from testing import get_test_db
+from src import app
 from src.database import UsersTable, GamesTable, CardsTable, ShowedCardsTable, __SessionLocal__
 from src.endpoints.create_game import create_game, CreateGameModel
 from src.endpoints.create_user import create_user
@@ -18,4 +19,72 @@ from src.endpoints.start_game import start_game
 
 def test_play_card(get_test_db):
     db = get_test_db
-    pass
+    client = TestClient(app)
+
+    nickname1 = 'andy'
+    response = client.post(f'/games/playing/{nickname1}?discard={True}&card_name={"Lanzallamas"}&target={"pepe"}')
+    assert response.status_code == 404
+    assert response.json()['detail'] == f"No existe un Usuario con nickname {nickname1}"
+
+    response = client.post(f'/users?nickname={nickname1}')
+    response = client.post(f'/games/playing/{nickname1}?discard={True}&card_name={"Lanzallamas"}&target={"pepe"}')
+    assert response.status_code == 400
+    assert response.json()['detail'] == f"Usuario {nickname1} no se encuentra en una Partida actualmente"
+
+    game_name = "Partida de Andy"
+    body = {"game_name": game_name, "min_players": 4, "max_players": 12, "creator": nickname1}
+    response = client.post(url="/games", json=body)
+    response = client.patch(f'/users/{nickname1}?game_name={game_name}')
+    response = client.post(f'/games/playing/{nickname1}?discard={True}&card_name={"Lanzallamas"}&target={"pepe"}')
+    assert response.status_code == 400
+    assert response.json()['detail'] == f"La Partida en la que se encuentre el Usuario {nickname1} aún no ha empezado"
+
+    nickname2 = 'pepe'
+    nickname3 = 'carla'
+    nickname4 = 'juan'
+    response = client.post(f'/users?nickname={nickname2}')
+    response = client.post(f'/users?nickname={nickname3}')
+    response = client.post(f'/users?nickname={nickname4}')
+    response = client.patch(f'/users/{nickname2}?game_name={game_name}')
+    response = client.patch(f'/users/{nickname3}?game_name={game_name}')
+    response = client.patch(f'/users/{nickname4}?game_name={game_name}')
+    response = client.post(f'/games/{nickname1}')
+
+    active_player = None
+    for user in response.json()['users']:
+        if user['active']:
+            active_player = user['nickname']
+    inactive_player = nickname1 if active_player != nickname1 else nickname2
+    active_player_card = None
+    for card in response.json()['cards']:
+        if card['owner'] == active_player:
+            active_player_card = card['name']
+
+    response = client.post(f'/games/playing/{inactive_player}?discard={True}&card_name={"Lanzallamas"}&target={"pepe"}')
+    assert response.status_code == 400
+    assert response.json()['detail'] == f"Usuario {inactive_player} no es el jugador activo"
+
+    response = client.post(f'/games/playing/{active_player}?discard={True}&card_name={active_player_card}')
+    assert response.status_code == 200
+    assert response.json()['play_response'] == {}
+
+    next_user = None
+    new_active_player = None
+    old_active_player = active_player
+    for user in response.json()['users']:
+        if user['active']:
+            assert user['nickname'] != old_active_player
+            new_active_player = user['nickname']
+        if user['nickname'] == old_active_player:
+            next_user = user['next_user']
+    assert new_active_player == next_user
+
+    old_active_player_card_count = 0
+    new_active_player_card_count = 0
+    for card in response.json()['cards']:
+        if card['owner'] == old_active_player:
+            old_active_player_card_count += 1
+        if card['owner'] == new_active_player:
+            new_active_player_card_count += 1
+    assert old_active_player_card_count == 4
+    assert new_active_player_card_count == 5
