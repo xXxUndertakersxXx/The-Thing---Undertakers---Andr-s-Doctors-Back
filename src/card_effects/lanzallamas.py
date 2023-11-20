52f829245614e9024b22a655a6b9eb4d17468a75
andresDoctors <andresdaviddoctors@gmail.com>
2023-11-20 11:15:19 -0300

    Lanzallamas

--- src/card_effects/lanzallamas.py
+++ src/card_effects/lanzallamas.py
@@ -6,4 +6,16 @@ from src.database import UsersTable, GamesTable, CardsTable, ShowedCardsTable, g
 
 
 def lanzallamas(user_nickname: str, target_nickname: str, game_name: str, db: Session):
-    pass
+    target = db.get(UsersTable, target_nickname)
+    target.alive = False
+
+    users = []
+    user = db.get(UsersTable, target.next_user)
+    while user != target:
+        users.append(user)
+        user = db.get(UsersTable, user.next_user)
+
+    for i in range(-1, len(users) - 1):
+        users[i].next_user = users[i+1].nickname
+
+    db.commit()
