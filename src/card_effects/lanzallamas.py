dd3a3bb6dc0faec7a5533c56095aa3583b05479d
andresDoctors <andresdaviddoctors@gmail.com>
2023-11-23 09:02:03 -0300

    Update lanzallamas.py

--- src/card_effects/lanzallamas.py
+++ src/card_effects/lanzallamas.py
@@ -1,8 +1,34 @@
 from fastapi import HTTPException, Depends, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
+from typing import List
 
 from src.database import UsersTable, GamesTable, CardsTable, ShowedCardsTable, get_db
+
+
+def check_end_game(db: Session, game_name):
+    alive_humans: List[UsersTable] = (
+        db
+        .query(UsersTable)
+        .filter(UsersTable.game_name == game_name)
+        .filter(UsersTable.alive == True)
+        .filter(UsersTable.role == 'Human')
+        .all()
+    )
+
+    the_thing: UsersTable = (
+        db
+        .query(UsersTable)
+        .filter(UsersTable.game_name == game_name)
+        .filter(UsersTable.role == 'The Thing')
+        .first()
+    )
+
+    if alive_humans == [] or the_thing.alive == False:
+        game: GamesTable = db.get(GamesTable, game_name)
+        game.phase = 'Finished'
+
+        db.commit()
 
 
 def lanzallamas(user_nickname: str, target_nickname: str, game_name: str, db: Session):
@@ -21,4 +47,5 @@ def lanzallamas(user_nickname: str, target_nickname: str, game_name: str, db: Se
     target.alive = False
     target.next_user = None
 
+    check_end_game(db, game_name)
     db.commit()
